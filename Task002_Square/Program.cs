bb2cb1fa0ddaba1459ec502a6ab205ee1a3d23a8
katerina <you-like-me@yandex.ru>
2022-08-05 22:33:50 +0300

    Add Task 002

---
+++ Task002_Square/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число b ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a == b*b)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
