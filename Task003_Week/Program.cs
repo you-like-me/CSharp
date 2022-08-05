e33c722e9d506a2ab4cf1cf097daa7376f44d79a
katerina <you-like-me@yandex.ru>
2022-08-05 22:34:44 +0300

    Add Task 003

---
+++ Task003_Week/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int n = Convert.ToInt32(Console.ReadLine());
+switch (n)
+{
+    case 1:
+    {
+        Console.WriteLine("Понедельник");
+        break;
+    }
+    case 2:
+    {
+        Console.WriteLine("Вторник");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("Среда");
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("Четверг");
+        break;
+    }
+    case 5:
+    {
+        Console.WriteLine("Пятница");
+        break;
+    }
+    case 6:
+    {
+        Console.WriteLine("Суббота");
+        break;
+    }
+    case 7:
+    {
+        Console.WriteLine("Воскреселье");
+        break;
+    }
+
+}
